73063006873d018f8d0ee58549d8b3200858476d
Suboptimal Engineer <suboptimaleng@gmail.com>
2024-04-19 19:51:31 -0400

    Remove unneeded code.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { ThreeSceneV2 } from './ThreeSceneV2';
 import { ThreeSceneV3 } from './ThreeSceneV3';
 
 function App() {
-  const v1 = false;
   const version = 'v3';
   const map = {
     v1: <ThreeSceneV1 />,
